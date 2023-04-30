b2faf42215491a180de57f90f622581d272f3064
Ha Quang Minh <haquangminhbnvn@gmail.com>
2023-04-30 09:26:40 +0700

    add format dependences

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,18 +1,19 @@
-import {Button as AntdButton, ConfigProvider} from "antd";
+import { Button as AntdButton, ConfigProvider } from "antd";
 
 const theme = {
-    components: {
-        Button: {
-            colorBgContainer: '#E6F5FA',
-
-        }
-    }
-}
+  components: {
+    Button: {
+      colorBgContainer: "#E6F5FA",
+    },
+  },
+};
 
 const Button = (props: any) => {
-    return <ConfigProvider theme={theme}>
-        <AntdButton {...props} />
+  return (
+    <ConfigProvider theme={theme}>
+      <AntdButton {...props} />
     </ConfigProvider>
-}
+  );
+};
 
 export default Button;
