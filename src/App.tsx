6ac641aa246499928a45e5f9714165e3a07d948a
Ha Quang Minh <haquangminhbnvn@gmail.com>
2023-04-30 09:26:40 +0700

    add format dependences

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import 'antd/dist/reset.css'
-import './App.css';
-import {Route, Routes} from "react-router-dom";
+import React from "react";
+import logo from "./logo.svg";
+import "antd/dist/reset.css";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import { ConfigProvider } from "antd";
 import Button from "./components/common/Button";
 import SignIn from "./components/signin/SignIn";
 
 const theme = {
-    token: {
-        colorPrimary: '#005773',
-        colorLink: '#005773',
-        colorLinkHover: '#007da5',
-        colorLinkActive: '#00374d'
-    }
-}
+  token: {
+    colorPrimary: "#005773",
+    colorLink: "#005773",
+    colorLinkHover: "#007da5",
+    colorLinkActive: "#00374d",
+  },
+};
 
 function App() {
-    return <ConfigProvider theme={theme}>
-        <Routes>
-            <Route path="/" element={ <SignIn/> }/>
-            <Route path="*" element={<div>404</div>} />
-        </Routes>
-    </ConfigProvider>;
+  return (
+    <ConfigProvider theme={theme}>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="*" element={<div>404</div>} />
+      </Routes>
+    </ConfigProvider>
+  );
 }
 
 export default App;
